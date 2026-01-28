d4a892dcf23261209d9b76744750aed02e9e3e42
Md Nesad <nesadm26@gmail.com>
2026-01-28 15:20:37 +0600

    added not found and error handaling middleware

--- server.js
+++ server.js
@@ -11,13 +11,19 @@ app.use(express.json());
 //connect db
 connectDb();
 
-app.get("/", (req, res) => res.send("dfjasjdfkjd"));
+app.get("/", (req, res) => res.send(a));
 
 app.use("/api", userRouter);
 
+//404 not found middleware
+app.use((req, res, next) => {
+  return res.status(404).json({ message: "Route not found" });
+});
+
+//error middleware
 app.use((err, req, res, next) => {
-  if (err) {
-    return res.status(500).json({ message: err.message });
+  if (err.message) {
+    res.status(500).json({ message: err.message });
   } else {
     res.status(500).json({ message: "Something went wrong" });
   }
